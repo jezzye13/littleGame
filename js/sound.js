301933e4bef03fd52b2f5f2485a74f844647c5f3
jezzye13 <jd.jessey@gmail.com>
2014-02-24 19:46:14 +0100

    sound
    fix

---
+++ js/sound.js
@@ -0,0 +1,21 @@
+var shoot = new Audio();
+shoot.src = "snd/shoot.mp3";
+
+var win = new Audio();
+win.src = "snd/win.mp3";
+
+var lose = new Audio();
+lose.src = "snd/lose.mp3";
+
+var bgsound = Audio();
+bgsound.src = "snd/bg.mp3";
+bgsound.load();
+bgsound.volume = 0.3;
+
+var laser = new Audio();
+laser.src = "snd/laser.mp3";
+
+var bossWin = new Audio();
+bossWin.src = "snd/bossWin.wav";
+bossWin.load();
+bossWin.volume = 1.0;
